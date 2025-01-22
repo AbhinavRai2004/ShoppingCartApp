a85f060597eef3be03192d7a53ea0e5cab62e607
Abhinav <raiabhinav.in@gmail.com>
2025-01-22 17:56:22 +0530

    Category & Product Repository created

---
+++ src/main/java/com/example/shoppingcart/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.example.shoppingcart.repository;
+
+import com.example.shoppingcart.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByName(String name);
+}
---
+++ src/main/java/com/example/shoppingcart/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.example.shoppingcart.repository;
+
+import com.example.shoppingcart.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryName(String category);
+
+    List<Product> findByBrand(String brand);
+
+    List<Product> findByCategoryNameAndBrand(String category, String brand);
+
+    List<Product> findByName(String name);
+
+    List<Product> findByBrandAndName(String brand, String name);
+
+    long countByBrandAndName(String brand, String name);
+}
