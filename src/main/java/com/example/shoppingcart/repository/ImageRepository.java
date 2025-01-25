ede34e4389883b804472825d05c830da2815fa8c
Abhinav <raiabhinav.in@gmail.com>
2025-01-25 10:24:20 +0530

    ImageService class implemented

---
+++ src/main/java/com/example/shoppingcart/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.example.shoppingcart.repository;
+
+import com.example.shoppingcart.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findByProductId(Long id);
+}
---
+++ src/main/java/com/example/shoppingcart/service/image/IImageService.java
@@ -0,0 +1,14 @@
+package com.example.shoppingcart.service.image;
+
+import com.example.shoppingcart.dto.ImageDto;
+import com.example.shoppingcart.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IImageService {
+    Image getImageById(Long id);
+    void deleteImageById(Long id);
+    List<ImageDto> saveImages(Long productId, List<MultipartFile> files);
+    void updateImage(MultipartFile file,  Long imageId);
+}
